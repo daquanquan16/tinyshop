4fd915ca27727014d0313e4cd063bf635f2ff1ae
tiny.huawei <tinyHuaWei@.com>
2021-03-24 10:29:22 +0800

    修改

--- src/tinyshop/redis/Test.php
+++ src/tinyshop/redis/Test.php
@@ -9,7 +9,7 @@ class Test
     public function index()
     {
        $obj =\tinyshop\redis\Sdk::getInstance();
-   return     $redis=$obj->getRedis();
+         return     $redis=$obj->getRedis();
     }
 
 }
@@ -17,5 +17,10 @@ include_once ('./Config.php');
 include_once ('./Sdk.php');
 $obj=new Test();
 $redis=$obj->index();
-print_r($redis->set("tests",11));
-print_r($redis->get("tests"));
+$order="orderkey";
+$result=$redis->ZADD($order,time(),2);
+$result=$redis->ZADD($order,time(),3);
+$result=$redis->ZRANGE($order,0,time(),'WITHSCORES');
+print_r($result);
+/*print_r($redis->set("tests",11));
+print_r($redis->get("tests"));*/
